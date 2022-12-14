a6ffc79f8df2ec5393f7ccd886fb6e8e175479da
lasineitor <120582942+lasineitor@users.noreply.github.com>
2022-12-14 17:26:14 +0100

    Update modulo.py

--- modulo.py
+++ modulo.py
@@ -1,1 +1,14 @@
 
+#Módulo Matrices.
+
+#Definición matrices:
+
+
+
+class matriz:
+  def __init__(self,lista):
+    self.matriz = lista
+    self.filas = len(lista)
+    self.columnas = len(lista[0])
+
+A = matriz([2,3],[1,2])
