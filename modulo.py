c12d6b7b0c351152521e6f154f77246e25bcf817
NotTurtle01 <120562401+NotTurtle01@users.noreply.github.com>
2022-12-14 17:36:50 +0100

    Update modulo.py

--- modulo.py
+++ modulo.py
@@ -1,8 +1,6 @@
-
 #Módulo Matrices.
 
 #Definición matrices:
-
 
 
 class matriz:
@@ -10,5 +8,9 @@ class matriz:
     self.matriz = lista
     self.filas = len(lista)
     self.columnas = len(lista[0])
+    
+    for fila in lista:
+      if len(fila) != len(lista[0]):
+        print('Tu lista no es una matriz')
 
 A = matriz([2,3],[1,2])
