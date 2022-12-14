5fabaf1b473742335a56a4bce864f420cb041c88
NotTurtle01 <120562401+NotTurtle01@users.noreply.github.com>
2022-12-14 17:38:46 +0100

    Update modulo.py

--- modulo.py
+++ modulo.py
@@ -8,9 +8,10 @@ class matriz:
     self.matriz = lista
     self.filas = len(lista)
     self.columnas = len(lista[0])
-    
     for fila in lista:
       if len(fila) != len(lista[0]):
         print('Tu lista no es una matriz')
+   
+  def __str__
 
 A = matriz([2,3],[1,2])
