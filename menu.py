7705e2b727d13e4b183e11fe5db42951baca65d5
NotTurtle01 <120562401+NotTurtle01@users.noreply.github.com>
2022-12-19 16:51:11 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,12 @@
 #Menu
 
-#1. Definición de una matriz de unas dimensiones dadas.
+def menu():
+    opcion = 0
+    while opcion != 1 and opcion != 2:
+        print('1) Definición de una matriz de unas dimensiones dadas.')
+        print('2) Asignación de un elemento específico de una matriz')
+        opcion = int(input('Escoge opción: '))
+    return opcion
+
+
 
