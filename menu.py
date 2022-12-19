e2d4f29d884097d25836b47430779037de3c5900
NotTurtle01 <120562401+NotTurtle01@users.noreply.github.com>
2022-12-19 16:53:18 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -3,9 +3,10 @@
 def menu():
     opcion = 0
     while opcion != 1 and opcion != 2:
-        print('1) Definición de una matriz de unas dimensiones dadas.')
+        print('1) Definición de una matriz de unas dimensiones dadas')
         print('2) Asignación de un elemento específico de una matriz')
         print('3) Obtención de un elemento específico de una matriz')
+        print('4) Presentación de una matriz por pantalla')
         opcion = int(input('Escoge opción: '))
     return opcion
 
