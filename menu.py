dab98606538e5926aeb4252f9ad47b04e0b8b3b8
NotTurtle01 <120562401+NotTurtle01@users.noreply.github.com>
2022-12-21 17:28:11 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -21,7 +21,7 @@ def guardar(lista):
 def menu():
     opcion = 0
     while opcion == 0:
-        print('1) Definición de una matriz de unas dimensiones dadas')
+        print('\n1) Definición de una matriz de unas dimensiones dadas')
         print('2) Asignación de un elemento específico de una matriz')
         print('3) Obtención de un elemento específico de una matriz')
         print('4) Presentación de una matriz por pantalla')
